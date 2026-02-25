853025781461ea01bd6c744339a750831b5eac7a
lrebuffi <lrebuffi@anl.gov>
2026-02-25 17:00:02 -0600

    Modified name of the scripts as indicated by S.Stepanov

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ import sys
 from setuptools import find_packages, setup
 
 NAME = 'OASYS2-SHADOW4'
-VERSION = '0.0.35'
+VERSION = '0.0.36'
 ISRELEASED = False
 
 DESCRIPTION = 'oasys2-shadow4: Oasys 2 widgets for shadow4'
@@ -84,7 +84,7 @@ CLASSIFIERS = [
 
 INSTALL_REQUIRES = (
     'oasys2>=0.0.39',
-    'shadow4>=0.1.77',
+    'shadow4>=0.1.79',
     'xoppylib>=1.0.51', # used in Bragg preprocessor... todo: maybe move that part to crystalpy?
 )
 
